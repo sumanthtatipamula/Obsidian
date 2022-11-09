60726663e8dfdfe2cd0abcabfbf6b9f575fc26fb
Sumanth, Tatipamula <sumanthtatipamula@gmail.com>
2022-11-09 09:16:32 +0530

    vault backup: 2022-11-09 09:16:32

--- Design Patterns/Creational/JavaCreationPatterns/src/PizzaFactory/stores/ChicagoPizzaStore.java
+++ Design Patterns/Creational/JavaCreationPatterns/src/PizzaFactory/stores/ChicagoPizzaStore.java
@@ -1,10 +1,27 @@
 package PizzaFactory.stores;
 
+import PizzaFactory.Chicago.ChicagoStyleCheesePizza;
+import PizzaFactory.Chicago.ChicagoStyleClamPizza;
+import PizzaFactory.Chicago.ChicagoStylePepperoniPizza;
+import PizzaFactory.Chicago.ChicagoStyleVeggiePizza;
 import PizzaFactory.Pizza;
 
 public class ChicagoPizzaStore extends PizzaStore{
     @Override
     Pizza createPizza(String type) {
-        return null;
+        Pizza pizza = null;
+        if(type.equals("cheese")){
+            pizza =  new ChicagoStyleCheesePizza();
+        }
+        else if(type.equals("pepperoni")){
+            pizza = new ChicagoStylePepperoniPizza();
+        }
+        else if(type.equals("clam")){
+            pizza = new ChicagoStyleClamPizza();
+        }
+        else if(type.equals("veggie")){
+            pizza = new ChicagoStyleVeggiePizza();
+        }
+        return pizza;
     }
 }
--- Design Patterns/Creational/JavaCreationPatterns/src/PizzaFactory/stores/NYPizzaStore.java
+++ Design Patterns/Creational/JavaCreationPatterns/src/PizzaFactory/stores/NYPizzaStore.java
@@ -1,10 +1,27 @@
 package PizzaFactory.stores;
 
+import PizzaFactory.NY.NYStyleCheesePizza;
+import PizzaFactory.NY.NYStylePepperoniPizza;
+import PizzaFactory.NY.NYStyleVeggiePizza;
+import PizzaFactory.NY.NyStyleClamPizza;
 import PizzaFactory.Pizza;
 
 public class NYPizzaStore extends PizzaStore{
     @Override
     Pizza createPizza(String type) {
-        return null;
+        Pizza pizza = null;
+        if(type.equals("cheese")){
+            pizza =  new NYStyleCheesePizza();
+        }
+        else if(type.equals("pepperoni")){
+            pizza = new NYStylePepperoniPizza();
+        }
+        else if(type.equals("clam")){
+            pizza = new NyStyleClamPizza();
+        }
+        else if(type.equals("veggie")){
+            pizza = new NYStyleVeggiePizza();
+        }
+        return pizza;
     }
 }
